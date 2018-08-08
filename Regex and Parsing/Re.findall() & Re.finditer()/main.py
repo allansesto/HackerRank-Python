9ad91141dbdd731cd7291a0b27f2797a85436edb
Allan Sesto <38048543+allansesto@users.noreply.github.com>
2018-08-07 22:46:19 -0300

    Update main.py

--- Regex and Parsing/Re.findall() & Re.finditer()/main.py
+++ Regex and Parsing/Re.findall() & Re.finditer()/main.py
@@ -4,4 +4,4 @@ if __name__ == "__main__":
     str = input()
     cons = '[qwrtypsdfghjklzxcvbnm]'
     tab = re.findall('(?<=' + cons +')([aeiou]{2,})' + cons, str, re.I)
-    print('\n'.join(tab or ['-1']))²
+    print('\n'.join(tab or ['-1']))
